23b2531551cdb82806a6babca7f8a17778ff12e8
Santiago Lezica <salezica@gmail.com>
2023-06-29 00:48:07 -0300

    Change argument format

--- src/cli.go
+++ src/cli.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+const DEFAULT_ENCODER = "raw"
+const DEFAULT_DECODER = "raw"
+
 type Command struct {
 	decoder Codec
 	encoder Codec
@@ -14,29 +17,52 @@ type Command struct {
 func ParseCommand(raw_args []string) (*Command, error) {
 	flagSet := flag.NewFlagSet("renc", flag.ExitOnError)
 	flagSet.Parse(raw_args)
-  args := flagSet.Args()
+	args := flagSet.Args()
 
 	var decoder Codec = &BinaryCodec{}
 	var encoder Codec = &BinaryCodec{}
 	var err error
 
-	if len(args) > 2 {
-		return nil, fmt.Errorf("Usage: renc [decoder=raw] [encoder=raw]\nError: too many arguments")
+	if len(args) == 0 {
+		return nil, fmt.Errorf("no [encoder]:[decoder] argument")
+	} else if len(args) > 1 {
+		return nil, fmt.Errorf("too many arguments. Did you mean '[encoder]:[decoder]' in a single argument?")
 	}
-	if len(args) > 0 {
-		decoder, err = CodecByName(args[0])
-		if err != nil {
-			return nil, fmt.Errorf("invalid decoder: %w", err)
-		}
+
+	codecNames, err := parseCodecPair(args[0])
+  if err != nil {
+    return nil, fmt.Errorf("invalid argument: %w", err)
+  }
+
+	decoder, err = CodecByName(codecNames[0])
+	if err != nil {
+		return nil, fmt.Errorf("invalid decoder: %w", err)
 	}
-	if len(args) > 1 {
-		encoder, err = CodecByName(args[1])
-		if err != nil {
-			return nil, fmt.Errorf("invalid encoder: %w", err)
-		}
+
+	encoder, err = CodecByName(codecNames[1])
+	if err != nil {
+		return nil, fmt.Errorf("invalid encoder: %w", err)
 	}
 
 	return &Command{decoder, encoder}, nil
+}
+
+func parseCodecPair(arg string) ([]string, error) {
+	names := strings.Split(arg, ":")
+
+	if len(names) != 2 {
+		return nil, fmt.Errorf("invalid '[encoder]:[decoder]' argument: %s", arg)
+	}
+
+	if names[0] == "" {
+		names[0] = DEFAULT_DECODER
+	}
+
+	if names[1] == "" {
+		names[1] = DEFAULT_ENCODER
+	}
+
+	return names, nil
 }
 
 var usage = `Usage: renc [decoder=raw] [encoder=raw]`
@@ -53,9 +79,9 @@ func Usage() string {
 }
 
 func Help() string {
-  return fmt.Sprintf("%s\n%s\n", usage, strings.TrimSpace(help))
+	return fmt.Sprintf("%s\n%s\n", usage, strings.TrimSpace(help))
 }
 
 func ErrorMsg(err error) string {
-  return fmt.Sprintf("Error: %s", err.Error())
+	return fmt.Sprintf("Error: %s", err.Error())
 }
